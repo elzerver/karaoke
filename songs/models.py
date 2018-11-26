1ceba94acaa8ecf4a44629c0356b029c12546cd3
Aldo <aldo.rivadeneira@gmail.com>
2018-11-26 10:52:45 -0600

    uploading the actual code, may need some changes and clean the tests

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Performer(models.Model):
+    name = models.CharField(max_length=255)
+    # performer = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    order = models.IntegerField(default=0)
+    performer = models.ForeignKey(Performer)
+
+    class Meta:
+        ordering = ['order',]
+    #duration = models.DurationField()
+
+    def __str__(self):
+        return '{} by {}'.format(self.title, self.artist)
