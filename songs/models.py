5c8ec966196f71bcb5d89dc9920b08d9e9e82bf9
Aldo <aldo.rivadeneira@gmail.com>
2021-10-24 11:40:01 +0200

    bug solved

--- songs/models.py
+++ songs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Performer(models.Model):
     name = models.CharField(max_length=255)
-    # performer = models.CharField(max_length=255, default='')
+    performer = models.CharField(max_length=255, default='')
 
     def __str__(self):
         return self.name
